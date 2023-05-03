6875068710835d48bea459fc2ac78123fdfb5d08
lemyppp <63813402+lemyppp@users.noreply.github.com>
2023-05-03 19:40:26 +0300

    new version

--- homeTaskMetaClasses.js
+++ homeTaskMetaClasses.js
@@ -1,91 +1,44 @@
+// Task 1: Code a Person class
 class Person {
-    constructor(name = 'Tom', age = 20, energy =100)
-    {this.name = name;
-    this.age = age;
-} sleep(){this.energy += 10;}
-doSomethingFun(){this.energy-=10;}
-} const person = new Person()
-console.log (person);
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+    }
+}
+
 // Task 2: Code a Worker class
 class Worker extends Person {
-    constructor(name, age, energyxp=0, hourlyWage = 10){
-        super (name, age, energy);
-        super (name,age,energy)
+    constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
         this.xp = xp;
         this.hourlyWage = hourlyWage;
     }
-    sleep(){super.sleep()}
-    doSomethingFun(){super.doSomethingFun}
-    goToWork(){
-        this.xp +=10;
+    goToWork() {
+        this.xp += 10;
     }
 }
-console.log(Worker);
-/*Task 1: Code a Person class
-Code a Person class, with three parameters in the constructor: name, age, and energy.
 
-Set the default parameters in the Person class as follows:
+// Task 3: Code an intern object, run methods
+function intern() {
+    var internObject = new Worker("Bob", 21, 110, 0, 10);
+    internObject.goToWork();
+    return internObject;
+}
 
-name = "Tom"
-
-age = 20
-
-energy = 100
-Code two methods in the Person class. Name those methods sleep() and doSomethingFun().
-
-The sleep() method should take the existing energy level and increase it by 10.
-
-The doSomethingFun() method should take the existing energy level and decrease it by 10.
+// Task 4: Code a manager object, methods
+function manager() {
+    var managerObject = new Worker("Alice", 30, 120, 100, 30);
+    managerObject.doSomethingFun();
+    return managerObject;
+}
 
 
-Task 2: Code a Worker class
-Code a sub-class, inheriting from the Person class, and name it Worker.
-
-The Worker class has two additional parameters in the constructor:
-
-xp (for "experience points")
-hourlyWage.
-These properties are set to the following default values:
-
-xp = 0
-
-hourlyWage = 10
-The Worker class has all the paramerters and methods of its super-class.
-
-Additionally, it has the goToWork() method, which, whenever it's run, increases the value of the xp property by 10.
-
-
-Task 3: Code a intern object
-Inside the intern function instantiate the Worker class to code a new intern object.
-
-The intern should have the following characteristics:
-
-name: Bob
-
-age: 21
-
-energy: 110
-
-xp: 0
-
-hourlyWage: 10
-Run the goToWork() method on the intern object. Then return the intern object.
-
-
-
-
-Task 4: Code a manager object
-Inside the manager function instantiate the Worker class to code a new manager object.
-
-The manager object should have the following characteristics:
-
-name: Alice
-
-age: 30
-
-energy: 120
-
-xp: 100
-
-hourlyWage: 30
-Run the doSomethingFun() method on the manager object. Then return the manager object.*/
+console.log(intern());
+console.log(manager());
